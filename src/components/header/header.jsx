27950af794393a3d54e35874b570571448427b52
Anastasia <555.555.555.anastasie@gmail.com>
2024-03-19 22:32:43 +0300

    animation version 2

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -7,37 +7,41 @@ import styles from './header.module.css';
 
 const Header = () => {
 
-  const animation = (selectedOptionExists) => {
+  function inputAnimation() {
+    const tl = gsap.timeline();
+    tl.to('#icon_input', { fill: '#fff', opacity: 1, duration: .1 });
+    tl.to('#input', { color: '#fff', opacity: 1, duration: .1})
+    .fromTo('#burger', { display: 'none', opacity: 0, duration:0 },{ display: 'flex', opacity: 1, duration:.5 })
+    tl.to('#list', { textAlign: 'center', opacity: 1, duration: 0 });
+    document.getElementById('input').blur();
+  }
 
+  const animation = (selectedOptionExists) => {
     const tl = gsap.timeline();
     if (selectedOptionExists) {
-        tl.to('#title', { display: 'none', opacity: 0 });
-        tl.fromTo(
-        '#input-wrapper',
-        { opacity: 1, y: 0, color: 'black' },
-        {
-          maxWidth: '162px',
-          y: 0,
-          backgroundColor: '#616161',
-          alignItems: 'end',
-          opacity: 1,
-          color: '#fff',
-        }
-        );
-        tl.to('#icon_input', { fill: '#fff', opacity: 1, duration: 0 });
-        tl.to('#list', {textAlign: 'center', opacity: 1, duration: 0 });
-        tl.to('#input', { color: '#fff', opacity: 1, duration: 0 });
+        tl.to('#title', { display: 'none', opacity: 0 }) ; 
         tl.to('#header', {
-        opacity: 1,
-        backgroundColor: '#464646',
-        height: '80px',
-        padding: '15px',
-        margin: 0,
-        alignItems: 'end'
-        }).fromTo('#burger', { display: 'none', opacity: 0 },
-         { display: 'flex', opacity: 1 });
-
-        document.getElementById('input').blur();
+          opacity: 1,
+          backgroundColor: '#464646',
+          height: '80px',
+          padding: '15px',
+          margin: 0,
+          alignItems: "end", 
+          justifyContent: "flex-end"
+        });
+        tl.fromTo(
+          '#input-wrapper',
+          { opacity: 1, y: 0, color: 'black' },
+          {
+            maxWidth: '162px',
+            y: 0,
+            backgroundColor: '#616161',
+            opacity: 1,
+            color: '#fff',
+          }
+          );
+        
+        tl.add(inputAnimation)
     }
   }
 
--- src/components/tooltip/tooltip.jsx
+++ src/components/tooltip/tooltip.jsx
@@ -13,6 +13,41 @@ const propTypes = {
 
 const Tooltip = ({title, position, children }) => {
 
+    const animation = (selectedOptionExists) => {
+
+        const tl = gsap.timeline();
+        if (selectedOptionExists) {
+            tl.to('#title', { display: 'none', opacity: 0 });
+            tl.fromTo(
+            '#input-wrapper',
+            { opacity: 1, y: 0, color: 'black' },
+            {
+              maxWidth: '162px',
+              y: 0,
+              backgroundColor: '#616161',
+              alignItems: 'end',
+              opacity: 1,
+              color: '#fff',
+            }
+            );
+            tl.to('#icon_input', { fill: '#fff', opacity: 1, duration: 0 });
+            tl.to('#list', {textAlign: 'center', opacity: 1, duration: 0 });
+            tl.to('#input', { color: '#fff', opacity: 1, duration: 0 });
+            tl.to('#header', {
+            opacity: 1,
+            backgroundColor: '#464646',
+            height: '80px',
+            padding: '15px',
+            margin: 0,
+            alignItems: 'end'
+            }).fromTo('#burger', { display: 'none', opacity: 0 },
+             { display: 'flex', opacity: 1 });
+    
+            document.getElementById('input').blur();
+        }
+      }
+    
+
   const node = useRef();
   const [isVisible, setState] = useState(false);
   const handleClick = ({ target }) => {
